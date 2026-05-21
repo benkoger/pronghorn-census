1099643aa956fcd896f217fc3442b4f40599bcfb
Michael Lance <mlance4@uwyo.edu>
2026-05-21 09:04:37 -0600

    everything says flask-caching supports redis over tls but it doesn't work!

--- airial_api/config.py
+++ airial_api/config.py
@@ -73,7 +73,9 @@ if (os.environ.get("SPICEDB_TLS") or "false").lower() in true_set:
 
 cache_redis_url = os.environ.get("CACHE_REDIS", "")
 
-print(cache_redis_url)
+from flask import current_app
+
+current_app.logger.info(cache_redis_url)
 
 cache_config = {
     "CACHE_TYPE": "RedisCache",
